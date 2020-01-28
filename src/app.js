b3439ca98e0a1840c0099b3d66f74c80abcd0d7c
Gizelly Steffanny <gizellysteffanny@gmail.com>
2020-01-28 09:54:33 -0400

    setup project

--- src/app.js
+++ src/app.js
@@ -1,1 +1,23 @@
-class App {}
+import express from 'express';
+
+import routes from './routes';
+
+class App {
+
+   constructor() {
+      this.server = express();
+
+      this.middlewares();
+      this.routes();
+   }
+
+   middlewares() {
+      this.server.use(express.json());
+   }
+
+   routes() {
+      this.server.use(routes);
+   }
+}
+
+export default new App().server;
